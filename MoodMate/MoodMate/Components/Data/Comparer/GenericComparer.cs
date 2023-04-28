2ef4f002f1fdca068510a0ca18ae48589035ef1d
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:19:15 +0300

    Create GenericComparer for SortByDate

---
+++ MoodMate/MoodMate/Components/Data/Comparer/GenericComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodMate.Components.Data.Comparer;
+
+sealed internal class GenericComparer<T> : IComparer<T>
+{
+    public int Compare(T x, T y)
+    {
+    }
+}
