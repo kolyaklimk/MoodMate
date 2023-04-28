285aa8a52e000c4b412b3ed2c67f773c5f7310a6
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:16:46 +0300

    DataControl implementation (Add,Change,Delete)

--- MoodMate/MoodMate/Components/Data/DataControl.cs
+++ MoodMate/MoodMate/Components/Data/DataControl.cs
@@ -1,5 +1,26 @@
-﻿namespace MoodMate.Components.Data;
+﻿using MoodMate.Components.Data.Abstractions;
+using System.Diagnostics;
 
-internal class DataControl
+namespace MoodMate.Components.Data;
+
+public class DataControl<T> : DataLoading<T>, IDataControl<T>
 {
+    public void Add(T item)
+    {
+        Data.Add(item);
+    }
+    public void Change(int index, T item)
+    {
+        Data[index] = item;
+    }
+    public void Delete(int index)
+    {
+        Data.RemoveAt(index);
+    }
+    public async Task SortByDate(string sortColumn)
+    {
+    }
+    public async Task UpdateFile(string path)
+    {
+    }
 }
