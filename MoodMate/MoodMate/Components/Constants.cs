edc454327fe7c4a9d3a2f5b5d54673f0e5b49751
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:06:57 +0300

    Create Constants class and add consts

---
+++ MoodMate/MoodMate/Components/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace MoodMate.Components;
+
+static class Constants
+{
+    public const string PathMoodNotes = "MoodNotes.json";
+    public const string PathMoods = "Moods.json";
+    public const string PathMusic = "Music.json";
+    public const string PathSound = "Sound.json";
+    public const string PathNotes = "Notes.json";
+    public const string SortByData = "Date";
+}
