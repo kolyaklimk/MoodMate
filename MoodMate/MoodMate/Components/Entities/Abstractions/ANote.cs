758c7ee4f3922caaaa122c9a79c8f4e4b7dac040
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:51:38 +0300

    Add methods to ANote with abstractioans methods

--- MoodMate/MoodMate/Components/Entities/Abstractions/ANote.cs
+++ MoodMate/MoodMate/Components/Entities/Abstractions/ANote.cs
@@ -1,5 +1,21 @@
-﻿namespace MoodMate.Components.Entities.Abstractions;
+﻿using MoodMate.Components.Data;
 
-internal class ANote
+namespace MoodMate.Components.Entities.Abstractions;
+
+internal abstract class ANote<T> where T : class
 {
+    protected DataControl<T> NoteControl { get; set; } = new();
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public string Text { get; set; }
+
+    public List<T> GetData()
+    {
+        return NoteControl.Data;
+    }
+    public abstract Task LoadNote();
+    public abstract Task AddNote(T obj);
+    public abstract Task ChangeNote(T obj, int id);
+    public abstract Task DeleteNote(int id);
+    public abstract void UpdateAllId();
 }
