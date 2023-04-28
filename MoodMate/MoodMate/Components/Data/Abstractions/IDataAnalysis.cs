72385ad721dbfa2ab789578b49d5289dfceddff8
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:02:05 +0300

    Update IDataAnalysis.cs

--- MoodMate/MoodMate/Components/Data/Abstractions/IDataAnalysis.cs
+++ MoodMate/MoodMate/Components/Data/Abstractions/IDataAnalysis.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MoodMate.Components.Data.Abstractions
+namespace MoodMate.Components.Data.Abstractions;
+
+internal interface IDataAnalysis<T>
 {
-    internal interface IDataAnalysis
-    {
-    }
+    public Dictionary<string, (string, int, int)> AnalysedData { get; set; }
+    void AddItem(string name, string source, DateTime date, DateTime Choosedate);
+    int GetCount();
+    void GetPercents();
 }
