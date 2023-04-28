caf0781239d878a5412839b99b889a45fdf237b2
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:04:10 +0300

    Update using in Interfaces

--- MoodMate/MoodMate/Components/Data/Abstractions/IDataAnalysis.cs
+++ MoodMate/MoodMate/Components/Data/Abstractions/IDataAnalysis.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MoodMate.Components.Data.Abstractions;
+﻿namespace MoodMate.Components.Data.Abstractions;
 
 internal interface IDataAnalysis<T>
 {
