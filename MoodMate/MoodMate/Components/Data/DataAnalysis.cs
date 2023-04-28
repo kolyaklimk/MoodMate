bce40517a8f146e9bf19f8f7328633707f170531
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:29:50 +0300

    Add Interface to DataAnalysis

--- MoodMate/MoodMate/Components/Data/DataAnalysis.cs
+++ MoodMate/MoodMate/Components/Data/DataAnalysis.cs
@@ -1,5 +1,18 @@
-﻿namespace MoodMate.Components.Data;
+﻿using MoodMate.Components.Data.Abstractions;
 
-internal class DataAnalysis
+namespace MoodMate.Components.Data;
+
+public class DataAnalysis<T> : IDataAnalysis<T>
 {
+    // Dictionary<name, (source, count of element, procent)> 
+    public Dictionary<string, (string, int, int)> AnalysedData { get; set; } = new();
+    public void AddItem(string name, string source, DateTime date, DateTime Choosedate)
+    {
+    }
+    public int GetCount()
+    {
+    }
+    public void GetPercents()
+    {
+    }
 }
