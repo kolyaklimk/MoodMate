9e7e070df94132b41c757acc95a451b1f1a44548
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:20:37 +0300

    GenericComparer implementation

--- MoodMate/MoodMate/Components/Data/Comparer/GenericComparer.cs
+++ MoodMate/MoodMate/Components/Data/Comparer/GenericComparer.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
 namespace MoodMate.Components.Data.Comparer;
 
 sealed internal class GenericComparer<T> : IComparer<T>
 {
+    private readonly string SortColumn;
+    public GenericComparer(string sortColumn)
+    {
+        SortColumn = sortColumn;
+    }
     public int Compare(T x, T y)
     {
+        PropertyInfo propertyInfo = typeof(T).GetProperty(SortColumn);
+        IComparable obj1 = (IComparable)propertyInfo.GetValue(x, null);
+        IComparable obj2 = (IComparable)propertyInfo.GetValue(y, null);
+        return obj2.CompareTo(obj1);
     }
 }
