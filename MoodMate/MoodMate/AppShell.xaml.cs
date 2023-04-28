041ee41935bdf484889a9540a4002ba796e3804e
Kolya <kolyaklimk@gmail.com>
2023-04-28 23:20:18 +0300

    Init C# Maui project

---
+++ MoodMate/MoodMate/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MoodMate;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MoodMate/MoodMate/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MoodMate;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
