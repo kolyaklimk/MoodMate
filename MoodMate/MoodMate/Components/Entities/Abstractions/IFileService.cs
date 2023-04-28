453e9c86df504c102cd4c4080d38c19e04f15c9c
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:49:39 +0300

    Rename "Load"

--- MoodMate/MoodMate/Components/Entities/Abstractions/IFileService.cs
+++ MoodMate/MoodMate/Components/Entities/Abstractions/IFileService.cs
@@ -5,5 +5,5 @@ internal interface IFileService
     string Name { get; set; }
     string Source { get; set; }
     string Description { get; set; }
-    Task Load(string path); 
+    Task LoadService(string path);
 }
