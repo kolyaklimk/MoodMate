fc204465120fa32d1818625cb7edd2449e9c6738
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:11:47 +0300

    DataLoading implementation

--- MoodMate/MoodMate/Components/Data/DataLoading.cs
+++ MoodMate/MoodMate/Components/Data/DataLoading.cs
@@ -1,5 +1,35 @@
-﻿namespace MoodMate.Components.Data;
+﻿using MoodMate.Components.Data.Abstractions;
+using System.Text.Json;
 
-internal class DataLoading
+namespace MoodMate.Components.Data;
+
+public class DataLoading<T> : IDataLoading<T>
 {
+    public List<T> Data { get; set; }
+    public async Task Load(string path, bool local)
+    {
+        try
+        {
+            string content;
+            if (local)
+            {
+                using Stream stream = await FileSystem.OpenAppPackageFileAsync(path);
+
+                using (StreamReader reader = new StreamReader(stream))
+                    content = await reader.ReadToEndAsync();
+            }
+            else
+            {
+                string targetFile = Path.Combine(FileSystem.Current.AppDataDirectory, path);
+
+                using (StreamReader reader = new StreamReader(targetFile))
+                    content = await reader.ReadToEndAsync();
+            }
+            Data = JsonSerializer.Deserialize<List<T>>(content);
+        }
+        catch
+        {
+            Data = new();
+        }
+    }
 }
