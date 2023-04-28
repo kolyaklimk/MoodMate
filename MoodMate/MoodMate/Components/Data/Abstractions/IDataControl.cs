3ef8c6144bb0fecf0a59eceb69da62ad9a697416
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:01:10 +0300

    Update IDataControl.cs

--- MoodMate/MoodMate/Components/Data/Abstractions/IDataControl.cs
+++ MoodMate/MoodMate/Components/Data/Abstractions/IDataControl.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MoodMate.Components.Data.Abstractions
+namespace MoodMate.Components.Data.Abstractions;
+
+internal interface IDataControl<T>
 {
-    internal interface IDataControl
-    {
-    }
+    void Delete(int index);
+    void Add(T item);
+    void Change(int index, T item);
+    Task SortByDate(string sortColumn);
+    Task UpdateFile(string path);
 }
