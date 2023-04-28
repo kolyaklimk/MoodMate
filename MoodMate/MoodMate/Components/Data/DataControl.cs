3c3aacb60ebb4442722c709b1dd9b9c597041865
Kolya <kolyaklimk@gmail.com>
2023-04-29 00:26:11 +0300

    DataComtrol implementation (update,  sort)

--- MoodMate/MoodMate/Components/Data/DataControl.cs
+++ MoodMate/MoodMate/Components/Data/DataControl.cs
@@ -1,5 +1,6 @@
 ﻿using MoodMate.Components.Data.Abstractions;
-using System.Diagnostics;
+using MoodMate.Components.Data.Comparer;
+using System.Text.Json;
 
 namespace MoodMate.Components.Data;
 
@@ -19,8 +20,22 @@ public class DataControl<T> : DataLoading<T>, IDataControl<T>
     }
     public async Task SortByDate(string sortColumn)
     {
+        await Task.Run(() => Data.Sort(new GenericComparer<T>(sortColumn)));
     }
     public async Task UpdateFile(string path)
     {
+        var json = JsonSerializer.Serialize(Data);
+
+        try
+        {
+            string targetFile = Path.Combine(FileSystem.Current.AppDataDirectory, path);
+            using (StreamWriter writer = new StreamWriter(targetFile, false))
+            {
+                await writer.WriteLineAsync(json);
+            }
+        }
+        catch
+        {
+        }
     }
 }
