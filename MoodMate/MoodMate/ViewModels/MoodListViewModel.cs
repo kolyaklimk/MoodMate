1b1c284d40901506a6b88a432b17a4f429a3bfcd
Kolya <kolyaklimk@gmail.com>
2023-04-29 13:08:43 +0300

    Create MoodListViewModel.cs

---
+++ MoodMate/MoodMate/ViewModels/MoodListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MoodMate.Components.Entities;
+using MoodMate.Components.Factory;
+using System.Collections.ObjectModel;
+
+namespace MoodMate.ViewModels;
+
+internal partial class MoodListViewModel: ObservableObject
+{
+    private readonly Note MoodNote;
+    public ObservableCollection<MoodNote> MoodNotes { get; set; } = new();
+    public MoodListViewModel()
+    {
+        MoodNote = new("Mood");
+        MoodNotes = MoodNote.note.GetData();
+        LoadMoodNote();
+    }
+
+    [RelayCommand]
+    async void LoadMoodNote() => await Load();
+    private async Task Load()
+    {
+        await MoodNote.note.LoadNote();
+    }
+}
