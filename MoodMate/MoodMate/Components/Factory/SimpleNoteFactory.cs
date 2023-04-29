938e4b48f374c5d6656f07201d3d827bbc44d431
Kolya <kolyaklimk@gmail.com>
2023-04-29 11:09:30 +0300

    Create SimpleNoteFactory and implementation INoteFactory

---
+++ MoodMate/MoodMate/Components/Factory/SimpleNoteFactory.cs
@@ -0,0 +1,12 @@
+﻿using MoodMate.Components.Entities.Abstractions;
+using MoodMate.Components.Entities;
+
+namespace MoodMate.Components.Factory;
+
+internal class SimpleNoteFactory : INoteFactory<SimpleNote>
+{
+    public ANote<SimpleNote> CreateNote()
+    {
+        return new SimpleNote();
+    }
+}
