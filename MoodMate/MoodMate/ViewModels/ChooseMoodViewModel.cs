fbbc2bb09a0ced727fbec014c0e6c34e340d506e
Kolya <kolyaklimk@gmail.com>
2023-04-29 14:31:14 +0300

    Add Moods property

--- MoodMate/MoodMate/ViewModels/ChooseMoodViewModel.cs
+++ MoodMate/MoodMate/ViewModels/ChooseMoodViewModel.cs
@@ -1,5 +1,31 @@
-﻿namespace MoodMate.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MoodMate.Components.Entities;
+using System.Collections.ObjectModel;
 
-internal class ChooseMoodViewModel
+namespace MoodMate.ViewModels;
+
+public partial class ChooseMoodViewModel: ObservableObject
 {
+    private readonly FileService Mood;
+    public ObservableCollection<FileService> Moods { get; set; } = new();
+    public ChooseMoodViewModel() 
+    {
+        Mood = new();
+        LoadMoodNote();
+    }
+
+    [RelayCommand]
+    async void LoadMoodNote() => await Load();
+    private async Task Load()
+    {
+        await Mood.LoadService();
+        var moods = Mood.GetServiceData();
+
+
+        Moods.Clear();
+        foreach(var mood in moods)
+            Moods.Add(mood);
+    }
+
 }
