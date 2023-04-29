6b8fe36f6b94d287dd8b47401923b1f486f3655d
Kolya <kolyaklimk@gmail.com>
2023-04-29 10:54:14 +0300

    SimpleNote implementation

--- MoodMate/MoodMate/Components/Entities/SimpleNote.cs
+++ MoodMate/MoodMate/Components/Entities/SimpleNote.cs
@@ -1,5 +1,53 @@
-﻿namespace MoodMate.Components.Entities;
+﻿using MoodMate.Components.Entities.Abstractions;
 
-internal class SimpleNote
+namespace MoodMate.Components.Entities;
+
+internal class SimpleNote : ANote<SimpleNote>
 {
+    public SimpleNote() { }
+    public SimpleNote(DateTime date, string text)
+    {
+        Date = date;
+        Text = text;
+    }
+    public override async Task LoadNote()
+    {
+        await NoteControl.Load(Constants.PathNotes, false);
+    }
+    public override async Task AddNote(SimpleNote obj)
+    {
+        NoteControl.Add(obj);
+        await NoteControl.SortByDate(Constants.SortByData);
+        UpdateAllId();
+        await NoteControl.UpdateFile(Constants.PathNotes);
+    }
+    public override async Task ChangeNote(SimpleNote obj, int id)
+    {
+        var index = NoteControl.Data.FindIndex(item => item.Id == id);
+        if (index > -1)
+        {
+            NoteControl.Change(index, obj);
+            await NoteControl.SortByDate(Constants.SortByData);
+            UpdateAllId();
+            await NoteControl.UpdateFile(Constants.PathNotes);
+        }
+    }
+    public override async Task DeleteNote(int id)
+    {
+        var index = NoteControl.Data.FindIndex(item => item.Id == id);
+        if (index > -1)
+        {
+            NoteControl.Delete(index);
+            await NoteControl.SortByDate(Constants.SortByData);
+            UpdateAllId();
+            await NoteControl.UpdateFile(Constants.PathNotes);
+        }
+    }
+    public override void UpdateAllId()
+    {
+        foreach (var item in NoteControl.Data.Select((value, index) => new { value, index }))
+        {
+            item.value.Id = item.index;
+        }
+    }
 }
