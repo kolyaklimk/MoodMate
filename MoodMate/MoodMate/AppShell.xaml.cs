99ffcc1dfccb37568b462f699afb636f50f88298
Kolya <kolyaklimk@gmail.com>
2023-04-29 14:59:45 +0300

    register ChooseMoodPage

--- MoodMate/MoodMate/AppShell.xaml.cs
+++ MoodMate/MoodMate/AppShell.xaml.cs
@@ -1,9 +1,12 @@
-﻿namespace MoodMate;
+﻿using MoodMate.Pages.MoodNote;
+
+namespace MoodMate;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+        Routing.RegisterRoute(nameof(ChooseMoodPage), typeof(ChooseMoodPage));
+    }
 }
