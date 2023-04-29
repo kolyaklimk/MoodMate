45c5592fe2e0c75f68cd20895a8648e6b23716d7
Kolya <kolyaklimk@gmail.com>
2023-04-29 11:01:43 +0300

    Create IMoodNoteAnalysis.cs

---
+++ MoodMate/MoodMate/Components/Entities/Abstractions/IMoodNoteAnalysis.cs
@@ -0,0 +1,12 @@
+﻿using MoodMate.Components.Data;
+
+namespace MoodMate.Components.Entities.Abstractions;
+
+internal interface INoteAnalysis
+{
+    DataAnalysis<MoodNote> MoodAnalysis { get; set; }
+    void InitAnalyse(DateTime date);
+    List<KeyValuePair<string, (string, int, int)>> GetAnalysedData();
+    int GetCountMood();
+    void FindPercentsMood();
+}
