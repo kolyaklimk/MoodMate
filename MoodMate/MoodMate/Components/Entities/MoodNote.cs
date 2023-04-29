eee87f07b49a5f7657d79c8a93e8e858c9def696
Kolya <kolyaklimk@gmail.com>
2023-04-29 10:56:09 +0300

    MoodNote implementation

--- MoodMate/MoodMate/Components/Entities/MoodNote.cs
+++ MoodMate/MoodMate/Components/Entities/MoodNote.cs
@@ -1,5 +1,60 @@
-﻿namespace MoodMate.Components.Entities;
+﻿using MoodMate.Components.Entities.Abstractions;
+using static System.Net.Mime.MediaTypeNames;
 
-internal class MoodNote
+namespace MoodMate.Components.Entities;
+
+internal class MoodNote : ANote<MoodNote>
 {
+    public FileService Mood { get; set; } = new();
+    public MoodNote() { }
+    public MoodNote(DateTime date, string name, string sourse, string text)
+    {
+        Date = date;
+        Mood.Name = name;
+        Mood.Source = sourse;
+        Text = text;
+    }
+
+    public override async Task LoadNote()
+    {
+        await NoteControl.Load(Constants.PathMoodNotes, false);
+    }
+
+    public override async Task AddNote(MoodNote obj)
+    {
+        NoteControl.Add(obj);
+        await NoteControl.SortByDate(Constants.SortByData);
+        UpdateAllId();
+        await NoteControl.UpdateFile(Constants.PathMoodNotes);
+    }
+    public override async Task ChangeNote(MoodNote obj, int id)
+    {
+        var index = NoteControl.Data.FindIndex(item => item.Id == id);
+        if (index > -1)
+        {
+            NoteControl.Change(index, obj);
+            await NoteControl.SortByDate(Constants.SortByData);
+            UpdateAllId();
+            await NoteControl.UpdateFile(Constants.PathMoodNotes);
+        }
+    }
+
+    public override async Task DeleteNote(int id)
+    {
+        var index = NoteControl.Data.FindIndex(item => item.Id == id);
+        if (index > -1)
+        {
+            NoteControl.Delete(index);
+            await NoteControl.SortByDate(Constants.SortByData);
+            UpdateAllId();
+            await NoteControl.UpdateFile(Constants.PathMoodNotes);
+        }
+    }
+    public override void UpdateAllId()
+    {
+        foreach (var item in NoteControl.Data.Select((value, index) => new { value, index }))
+        {
+            item.value.Id = item.index;
+        }
+    }
 }
