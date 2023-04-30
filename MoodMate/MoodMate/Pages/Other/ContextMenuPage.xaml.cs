c8f6f70eb4565b492ce2bc985868f0a6070f965d
Kolya <kolyaklimk@gmail.com>
2023-04-30 18:33:15 +0300

    Update ContextMenuPage.xaml.cs

--- MoodMate/MoodMate/Pages/Other/ContextMenuPage.xaml.cs
+++ MoodMate/MoodMate/Pages/Other/ContextMenuPage.xaml.cs
@@ -1,9 +1,16 @@
+using CommunityToolkit.Maui.Views;
+
 namespace MoodMate.Pages.Other;
 
-public partial class ContextMenuPage : ContentPage
+public partial class ContextMenuPage : Popup
 {
-	public ContextMenuPage()
-	{
-		InitializeComponent();
-	}
+    public ContextMenuPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Clicked_Share(object sender, EventArgs e) => Close(1);
+    private void Button_Clicked_Delete(object sender, EventArgs e) => Close(2);
+    private void Button_Clicked_Edit(object sender, EventArgs e) => Close(3);
+    private void Button_Clicked_Cancel(object sender, EventArgs e) => Close(4);
 }
