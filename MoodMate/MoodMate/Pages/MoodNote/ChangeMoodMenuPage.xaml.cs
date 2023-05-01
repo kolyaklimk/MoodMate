57876971d34dd7a3025a98b618d281ec84794e77
Kolya <kolyaklimk@gmail.com>
2023-05-01 20:33:53 +0300

    Update ChangeMoodMenu

--- MoodMate/MoodMate/Pages/MoodNote/ChangeMoodMenuPage.xaml.cs
+++ MoodMate/MoodMate/Pages/MoodNote/ChangeMoodMenuPage.xaml.cs
@@ -1,14 +1,21 @@
 using CommunityToolkit.Maui.Views;
-using MoodMate.ViewModels;
+using MoodMate.Components.Entities;
 
 namespace MoodMate.Pages.MoodNote;
 
 public partial class ChangeMoodMenuPage : Popup
 {
-	public ChangeMoodMenuPage(ChooseMoodViewModel model)
-	{
-		InitializeComponent();
-		BindingContext = model;
-	}
+    FileService Moods;
+    public ChangeMoodMenuPage(FileService[] fileServices)
+    {
+        InitializeComponent();
+        Moods = fileServices[0];
+        collection.ItemsSource = Moods.GetServiceData();
+    }
 
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        var item = ((CollectionView)sender).SelectedItem as FileService;
+        Close(new Tuple<string, string>(item.Source, item.Name));
+    }
 }
