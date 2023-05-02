1329e79447c08b2d205232d541b916a4e080f121
Kolya <kolyaklimk@gmail.com>
2023-05-02 22:16:45 +0300

    Update CreateOrEditNote

--- MoodMate/MoodMate/ViewModels/CreateOrEditNoteViewModel.cs
+++ MoodMate/MoodMate/ViewModels/CreateOrEditNoteViewModel.cs
@@ -1,5 +1,45 @@
-﻿namespace MoodMate.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MoodMate.Components.Entities;
+using MoodMate.Components.Factory;
 
-public partial class CreateOrEditNoteViewModel
+namespace MoodMate.ViewModels;
+
+
+[QueryProperty(nameof(SelectedNote), "SimpleNote")]
+[QueryProperty(nameof(Create), "Create")]
+public partial class CreateOrEditNoteViewModel : ObservableObject
 {
+    private readonly Note SimpleNote;
+    public CreateOrEditNoteViewModel(Note[] note)
+    {
+        SimpleNote = note[1];
+    }
+
+    [ObservableProperty] private bool create;
+    [ObservableProperty] SimpleNote selectedNote;
+
+    [RelayCommand]
+    async void CreateOrEdit()
+    {
+        if (SelectedNote.Text != "")
+        {
+            if (Create)
+            {
+                SelectedNote.Date = SelectedNote.Date.Date.Add(DateTime.Now.TimeOfDay);
+                SimpleNote.note.AddNote(SelectedNote);
+            }
+            else
+                SimpleNote.note.ChangeNote(SelectedNote, SelectedNote.Id);
+
+            MessagingCenter.Send(this, "UpdateSimpleNote");
+            await Shell.Current.Navigation.PopToRootAsync();
+        }
+    }
+
+    [RelayCommand]
+    async void Back_Clicked()
+    {
+        await Shell.Current.Navigation.PopAsync();
+    }
 }
