6e632de01c8ca2fa46988b9c14fa341a34e44462
Kolya <kolyaklimk@gmail.com>
2023-05-08 02:59:50 +0300

    Create RandomColorConverter.cs

---
+++ MoodMate/MoodMate/Converters/RandomColorConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace MoodMate.Converters;
+
+internal class RandomColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var colors = new string[] { "#2C3963", "#e2d9c5", "#8C913F" };
+        Random random = new Random();
+        return Color.FromArgb(colors[random.Next(0, colors.Length)]);
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
