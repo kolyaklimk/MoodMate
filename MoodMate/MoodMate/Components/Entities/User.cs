6f8e7087acde1740dea7c868b17c508201895ec5
Kolya <kolyaklimk@gmail.com>
2023-05-25 23:39:57 +0300

    Add new Alert and change SendEmailVerificationLink method

--- MoodMate/MoodMate/Components/Entities/User.cs
+++ MoodMate/MoodMate/Components/Entities/User.cs
@@ -9,7 +9,7 @@ using MoodMate.Components.Entities.Abstractions;
 
 namespace MoodMate.Components.Entities;
 
-public class User: IUser
+public class User : IUser
 {
     public FirebaseAuthClient Client { get; set; }
     public MimeMessage EmailMessage { get; set; }
@@ -28,23 +28,32 @@ public class User: IUser
         EmailMessage.From.Add(new MailboxAddress("MoodMate", PrivateConstants.Email));
         EmailMessage.Subject = "Verify your email address";
 
-        Alerts = new IToast[3];
+        Alerts = new IToast[4];
         Alerts[0] = Toast.Make("Check your email for verification!", ToastDuration.Short, 16);
         Alerts[1] = Toast.Make("Wrong email or password!", ToastDuration.Short, 16);
         Alerts[2] = Toast.Make("No internet connection!", ToastDuration.Short, 16);
+        Alerts[3] = Toast.Make("Email is not verified, try again in 1 minute to resend the mail!", ToastDuration.Short, 16);
     }
 
     public async Task<bool> SingIn(string email, string password)
     {
-        if(Connectivity.Current.NetworkAccess == NetworkAccess.Internet)
+        if (Connectivity.Current.NetworkAccess == NetworkAccess.Internet)
         {
             try
             {
                 await Client.SignInWithEmailAndPasswordAsync(email, password);
 
-                if(!Client.User.Info.IsEmailVerified)
+                if (!Client.User.Info.IsEmailVerified)
                 {
-                    await Alerts[0].Show();
+                    try
+                    {
+                        await SendEmailVerificationLink(email);
+                        await Alerts[0].Show();
+                    }
+                    catch
+                    {
+                        await Alerts[3].Show();
+                    }
                 }
                 else
                 {
@@ -66,7 +75,7 @@ public class User: IUser
 
     public async Task SingUp(string email, string password)
     {
-        if(Connectivity.Current.NetworkAccess == NetworkAccess.Internet)
+        if (Connectivity.Current.NetworkAccess == NetworkAccess.Internet)
         {
             try
             {
@@ -74,9 +83,7 @@ public class User: IUser
 
                 try
                 {
-                    var link = await FirebaseAuth.DefaultInstance.GenerateEmailVerificationLinkAsync(email);
-                    await Task.Run(() => SendEmail(email, link));
-
+                    await SendEmailVerificationLink(email);
                     await Alerts[0].Show();
                 }
                 catch
@@ -95,21 +102,26 @@ public class User: IUser
         }
     }
 
-    public void SendEmail(string email, string link)
+    public async Task SendEmailVerificationLink(string email)
     {
-        EmailMessage.To.Add(new MailboxAddress("", email));
-        EmailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-        {
-            Text = "<h2><a href=" + link + ">Сlick here</a> to verify your email address.</h2><br>"
-        };
+        var link = await FirebaseAuth.DefaultInstance.GenerateEmailVerificationLinkAsync(email);
 
-        using(var smtpClient = new SmtpClient())
+        await Task.Run(() =>
         {
-            smtpClient.ServerCertificateValidationCallback = (s, c, h, e) => true;
-            smtpClient.Connect("smtp.yandex.ru", 465, true);
-            smtpClient.Authenticate(PrivateConstants.Email, PrivateConstants.EmailPassword);
-            smtpClient.Send(EmailMessage);
-            smtpClient.Disconnect(true);
-        }
+            EmailMessage.To.Add(new MailboxAddress("", email));
+            EmailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = "<h2><a href=" + link + ">Сlick here</a> to verify your email address.</h2><br>"
+            };
+
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                smtpClient.Connect("smtp.yandex.ru", 465, true);
+                smtpClient.Authenticate(PrivateConstants.Email, PrivateConstants.EmailPassword);
+                smtpClient.Send(EmailMessage);
+                smtpClient.Disconnect(true);
+            }
+        });
     }
 }
