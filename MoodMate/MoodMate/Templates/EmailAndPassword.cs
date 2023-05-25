20d72db4114567854b7c48f6550b01c9b6a977ed
Kolya <kolyaklimk@gmail.com>
2023-05-26 00:21:54 +0300

    Update EmailAndPassword.cs

--- MoodMate/MoodMate/Templates/EmailAndPassword.cs
+++ MoodMate/MoodMate/Templates/EmailAndPassword.cs
@@ -4,4 +4,10 @@ public class EmailAndPassword
 {
     public string Email { get; set; }
     public string Password { get; set; }
+
+    public EmailAndPassword(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
 }
