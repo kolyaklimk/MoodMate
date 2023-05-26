0952f0c3a72cc5796c20c1fbfaffc2a34eb7d774
Kolya <kolyaklimk@gmail.com>
2023-05-27 01:09:26 +0300

    Implementation Firestore db for MoodNote

--- MoodMate/MoodMate/Components/Entities/MoodNote.cs
+++ MoodMate/MoodMate/Components/Entities/MoodNote.cs
@@ -1,4 +1,6 @@
-﻿using MoodMate.Components.Data;
+﻿using FirebaseAdmin;
+using Google.Cloud.Firestore;
+using MoodMate.Components.Data;
 using MoodMate.Components.Entities.Abstractions;
 using MoodMate.Templates;
 
@@ -8,7 +10,10 @@ public class MoodNote : ANote<MoodNote>, IMoodNoteAnalysis
 {
     public DataAnalysis<MoodNote> MoodAnalysis { get; set; } = new();
     public FileService Mood { get; set; } = new();
-    public MoodNote() { }
+    public MoodNote()
+    {
+        Db = FirestoreDb.Create(FirebaseApp.DefaultInstance.Options.ProjectId);
+    }
     public MoodNote(DateTime date, string name, string sourse, string text)
     {
         Date = date;
@@ -20,33 +25,87 @@ public class MoodNote : ANote<MoodNote>, IMoodNoteAnalysis
     {
         return NoteControl.Data.OrderByDescending(x => x.Date).ToList();
     }
-    public override async Task LoadNote()
+    public override async Task LoadNoteLocal()
     {
         await NoteControl.Load(Constants.PathMoodNotes, false);
     }
-    public override async Task AddNote(MoodNote obj)
+    public override async Task LoadNoteLocalToCloud(string uid)
+    {
+        var lastId = NoteControl.Data.Last().Id;
+
+        foreach (var item in NoteControl.Data.Select((value, index) => new { value, index }))
+        {
+            item.value.Id = (uint)item.index + lastId;
+            await Db.Collection("Users").Document(uid).Collection("MoodNote").Document(item.value.Id.ToString()).SetAsync(item);
+        }
+
+        NoteControl.Data.Clear();
+        await NoteControl.UpdateFile(Constants.PathMoodNotes);
+    }
+    public override async Task LoadNoteCloud(string uid)
+    {
+        var snapshot = await Db.Collection("Users").Document(uid).Collection("MoodNote").GetSnapshotAsync();
+
+        NoteControl.Data.Append(snapshot.Documents.Select(doc =>
+        {
+            return new MoodNote
+            {
+                Id = doc.GetValue<uint>("id"),
+                Date = doc.GetValue<DateTime>("date"),
+                Text = doc.GetValue<string>("text"),
+                Mood = new()
+                {
+                    Name = doc.GetValue<string>("name"),
+                    Source = doc.GetValue<string>("source"),
+                }
+            };
+        }).ToList());
+    }
+    public override async Task AddNote(MoodNote obj, string uid = null)
     {
         if (NoteControl.Data.Count > 0)
             obj.Id = NoteControl.Data.Last().Id + 1;
-        NoteControl.Add(obj);
-        await NoteControl.UpdateFile(Constants.PathMoodNotes);
-    }
-    public override async Task ChangeNote(MoodNote obj, uint id)
-    {
-        var index = NoteControl.Data.FindIndex(item => item.Id == id);
-        if (index > -1)
+
+        if (uid != null)
         {
-            NoteControl.Change(index, obj);
+            await Db.Collection("Users").Document(uid).Collection("MoodNote").Document(obj.Id.ToString()).SetAsync(obj);
+        }
+        else
+        {
+            NoteControl.Add(obj);
             await NoteControl.UpdateFile(Constants.PathMoodNotes);
         }
     }
-    public override async Task DeleteNote(uint id)
+    public override async Task ChangeNote(MoodNote obj, string uid = null)
     {
-        var index = NoteControl.Data.FindIndex(item => item.Id == id);
+        var index = NoteControl.Data.FindIndex(item => obj.Id == Id);
         if (index > -1)
         {
-            NoteControl.Delete(index);
-            await NoteControl.UpdateFile(Constants.PathMoodNotes);
+            if (uid != null)
+            {
+                await Db.Collection("Users").Document(uid).Collection("MoodNote").Document(obj.Id.ToString()).SetAsync(obj);
+            }
+            else
+            {
+                NoteControl.Change(index, obj);
+                await NoteControl.UpdateFile(Constants.PathMoodNotes);
+            }
+        }
+    }
+    public override async Task DeleteNote(MoodNote obj, string uid = null)
+    {
+        var index = NoteControl.Data.FindIndex(item => obj.Id == Id);
+        if (index > -1)
+        {
+            if (uid != null)
+            {
+                await Db.Collection("Users").Document(uid).Collection("MoodNote").Document(obj.Id.ToString()).DeleteAsync();
+            }
+            else
+            {
+                NoteControl.Delete(index);
+                await NoteControl.UpdateFile(Constants.PathMoodNotes);
+            }
         }
     }
 
