b2394a4d1e2eee27bada2537fa278bc04bafca48
Kolya <kolyaklimk@gmail.com>
2023-05-27 11:46:38 +0300

    Create CloudWarningPage.xaml.cs

---
+++ MoodMate/MoodMate/Pages/Other/CloudWarningPage.xaml.cs
@@ -0,0 +1,14 @@
+using CommunityToolkit.Maui.Views;
+
+namespace MoodMate.Pages.Other;
+
+public partial class CloudWarningPage : Popup
+{
+	public CloudWarningPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Offline_Clicked(object sender, EventArgs e) => Close(false);
+    private void Synchronize_Clicked(object sender, EventArgs e) => Close(true);
+}
