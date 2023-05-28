7ab0b62bb44f287d617f91cc44bbb6bcaf15fc24
Kolya <kolyaklimk@gmail.com>
2023-05-28 15:54:42 +0300

    Id to string and Rename LoadNoteCloudAndSaveLocal to SaveLocalToCloud

--- MoodMate/MoodMate/Components/Entities/Abstractions/ANote.cs
+++ MoodMate/MoodMate/Components/Entities/Abstractions/ANote.cs
@@ -8,7 +8,7 @@ public abstract partial class ANote<T> : ObservableObject
 {
     protected DataControl<T> NoteControl { get; set; } = new();
     protected FirestoreDb Db { get; set; }
-    [ObservableProperty] public uint id;
+    public string Id { get; set; }
     [ObservableProperty] public DateTime date;
     [ObservableProperty] public string text;
     public List<T> GetData()
@@ -21,7 +21,7 @@ public abstract partial class ANote<T> : ObservableObject
     }
     public abstract List<T> GetDataSortByDate();
     public abstract Task LoadNoteLocal();
-    public abstract Task LoadNoteCloudAndSaveLocal(Firebase.Auth.User user);
+    public abstract Task SaveLocalToCloud(Firebase.Auth.User user);
     public abstract Task LoadNoteCloud(Firebase.Auth.User user);
     public abstract Task AddNote(T obj, Firebase.Auth.User user = null);
     public abstract Task ChangeNote(T obj, Firebase.Auth.User user = null);
