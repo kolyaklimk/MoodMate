65f23c301932dab82846baff4ba8d923fec38ad2
Kolya <kolyaklimk@gmail.com>
2023-05-30 22:56:27 +0300

    Delete async

--- MoodMate/MoodMate/App.xaml.cs
+++ MoodMate/MoodMate/App.xaml.cs
@@ -4,33 +4,30 @@ using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
 using MoodMate.Components;
 using MoodMate.Components.Entities;
-using MoodMate.Components.Factory;
 
 namespace MoodMate;
 
 public partial class App : Application
 {
-    public App(Note[] notes, FileService[] fileServices, List<int>[] sbytes, IToast[] toasts)
+    public App(FileService[] fileServices, List<int>[] sbytes, IToast[] toasts)
     {
         InitializeComponent();
-        LoadeService(notes, fileServices, sbytes, toasts);
+        LoadeService(fileServices, sbytes, toasts);
         MainPage = new AppShell();
     }
-    private async void LoadeService(Note[] notes, FileService[] fileServices, List<int>[] sbytes, IToast[] toasts)
+    private async void LoadeService(FileService[] fileServices, List<int>[] sbytes, IToast[] toasts)
     {
         await fileServices[0].LoadService(Constants.PathMoods);
         await fileServices[1].LoadService(Constants.PathMusic);
         await fileServices[2].LoadService(Constants.PathSound);
-        sbytes[0] = await Task.Run(() => Enumerable.Range(0, 24).ToList());
-        sbytes[1] = await Task.Run(() => Enumerable.Range(0, 60).ToList());
 
-        await Task.Run(() =>
-        {
-            toasts[0] = Toast.Make("Please choose mood!", ToastDuration.Short, 16);
-            toasts[1] = Toast.Make("Please enter your mood!", ToastDuration.Short, 16);
-            toasts[2] = Toast.Make("Please fill out a note!", ToastDuration.Short, 16);
-            toasts[3] = Toast.Make("Please choose a sound or music!", ToastDuration.Short, 16);
-        });
+        sbytes[0] = Enumerable.Range(0, 24).ToList();
+        sbytes[1] = Enumerable.Range(0, 60).ToList();
+
+        toasts[0] = Toast.Make("Please choose mood!", ToastDuration.Short, 16);
+        toasts[1] = Toast.Make("Please enter your mood!", ToastDuration.Short, 16);
+        toasts[2] = Toast.Make("Please fill out a note!", ToastDuration.Short, 16);
+        toasts[3] = Toast.Make("Please choose a sound or music!", ToastDuration.Short, 16);
 
         var localPath = Path.Combine(FileSystem.CacheDirectory, "admin.json");
         using var json = await FileSystem.OpenAppPackageFileAsync("admin.json");
