d30b663af683c93e059afa4c548270fdede9e751
Kolya <kolyaklimk@gmail.com>
2023-06-01 03:10:32 +0300

    Create IDataCloud.cs

---
+++ MoodMate/MoodMate/Components/Data/Abstractions/IDataCloud.cs
@@ -0,0 +1,13 @@
+﻿using Google.Cloud.Firestore;
+
+namespace MoodMate.Components.Data.Abstractions;
+
+public interface IDataCloud
+{
+    FirestoreDb Db { get; set; }
+    Task<DocumentReference> AddAsync(string uid, string name, Dictionary<string, object> dict);
+    Task<QuerySnapshot> GetOrderByDateAsync(string uid, string name, int offset, int limit);
+    Task UpdateAsync(string uid, string name, string id, Dictionary<string, object> dict);
+    Task DeleteAsync(string uid, string name, string id);
+    Task<QuerySnapshot> GetintervalAsync(string uid, string name, DateTime firstDay, DateTime lastDay);
+}
