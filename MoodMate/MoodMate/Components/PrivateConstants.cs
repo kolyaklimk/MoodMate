55aac91064a1c1e8ee178f4483b64807b7c6e728
Kolya <kolyaklimk@gmail.com>
2023-06-15 10:47:21 +0300

    Add private constants and keys

---
+++ MoodMate/MoodMate/Components/PrivateConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace MoodMate.Components;
+
+static class PrivateConstants
+{
+    public const string Email = "Supplier mail";
+    public const string EmailPassword = "Supplier email password";
+    public const string ApiKey = "Firebase Web API Key";
+    public const string AuthDomain = "Firebase AuthDomain";
+    public const string ProjectId = "Firebase ProjectId";
+    public const string host = "Mail host";
+    public const int port = 000;
+    public const bool useSsl = true;
+}
